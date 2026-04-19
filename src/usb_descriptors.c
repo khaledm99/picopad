f0b52455e3d682d2e5d0140affd4d83dd9fcb6dd
Khaled <khaledmog99@yahoo.ca>
2026-04-19 17:31:48 -0600

    Added descriptor callbacks to usb_descriptors.c. Copied string descriptor code from tinyusb hid boot device example - understanding the intricacies of the string descriptors is outside the scope of this project

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -1,5 +1,7 @@
 // Device Descriptor
+#include "bsp/board_api.h"
 #include "common/tusb_types.h"
+#include "class/hid/hid_device.h"
 #include "class/hid/hid.h"
 #include "tusb.h"
 #include "tusb_config.h"
@@ -89,3 +91,85 @@ packed_descriptors_t const descriptors =
         .bInterval          = 1     // 1ms
     }
 };
+
+
+uint8_t const * tud_descriptor_device_cb(void)
+{
+    return (uint8_t const *) &desc_device;
+}
+
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
+{
+    return (uint8_t const *) &descriptors;
+}
+
+uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance)
+{
+    return desc_hid_keyboard_report;
+}
+
+//--------------------------------------------------------------------+
+// String Descriptors
+// Copied from tinyusb hid_boot_interface example
+//--------------------------------------------------------------------+
+
+// String Descriptor Index
+enum {
+  STRID_LANGID = 0,
+  STRID_MANUFACTURER,
+  STRID_PRODUCT,
+  STRID_SERIAL,
+};
+
+// array of pointer to string descriptors
+char const *string_desc_arr[] =
+{
+  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+  "Khaled Mograbee",                     // 1: Manufacturer
+  "Picopad",              // 2: Product
+  "01",                          // 3: Serials will use unique ID if possible
+};
+
+static uint16_t _desc_str[32 + 1];
+
+// Invoked when received GET STRING DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+  (void) langid;
+  size_t chr_count;
+
+  switch ( index ) {
+    case STRID_LANGID:
+      memcpy(&_desc_str[1], string_desc_arr[0], 2);
+      chr_count = 1;
+      break;
+
+    case STRID_SERIAL:
+      chr_count = board_usb_get_serial(_desc_str + 1, 32);
+      break;
+
+    default:
+      // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+      // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+
+      if ( !(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])) ) return NULL;
+
+      const char *str = string_desc_arr[index];
+
+      // Cap at max char
+      chr_count = strlen(str);
+      size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
+      if ( chr_count > max_count ) chr_count = max_count;
+
+      // Convert ASCII string into UTF-16
+      for ( size_t i = 0; i < chr_count; i++ ) {
+        _desc_str[1 + i] = str[i];
+      }
+      break;
+  }
+
+  // first byte is length (including header), second byte is string type
+  _desc_str[0] = (uint16_t) ((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
+
+  return _desc_str;
+}
